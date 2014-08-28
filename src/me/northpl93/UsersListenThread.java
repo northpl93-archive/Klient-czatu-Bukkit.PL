4d34710e1b0a105455054ff8f08026914ac25e6b
Michał <Micha@Michal-PC>
2014-08-28 18:40:05 +0200

    Optymalizacja pobierania listy userów na czacie

--- src/me/northpl93/UsersListenThread.java
+++ src/me/northpl93/UsersListenThread.java
@@ -1,13 +1,16 @@
 package me.northpl93;
 
+import me.northpl93.gui.ChatPanel;
+import me.northpl93.gui.PanelsEnum;
+import me.northpl93.utils.JsonUsersResponseParser;
+import me.northpl93.utils.PostExecute;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import me.northpl93.gui.PanelsEnum;
-import me.northpl93.utils.PostExecute;
-import me.northpl93.gui.ChatPanel;
+import com.google.gson.Gson;
 
 public class UsersListenThread extends Thread
 {
@@ -15,10 +18,14 @@ public class UsersListenThread extends Thread
 	@Override
 	public void run()
 	{
+		Gson gson = new Gson();
+		
 		while(true)
 		{
-			String content = PostExecute.excutePost("http://bukkit.pl/shoutbox", "_xfToken="+((ChatListenThread)Main.chatListener).getLoggedUser());
-			Document document = Jsoup.parse(content);
+			String content = PostExecute.excutePost("http://bukkit.pl/shoutbox", "_xfToken="+((ChatListenThread)Main.chatListener).getLoggedUser()+"&_xfResponseType=json");
+			JsonUsersResponseParser decoded_json = gson.fromJson(content, JsonUsersResponseParser.class);
+			
+			Document document = Jsoup.parse(decoded_json.getSidebarHtml());
 			
 			Elements as = document.getElementsByAttributeValue("class", "section membersOnline userList");
 			int i = 0;
---
+++ src/me/northpl93/utils/JsonUsersResponseParser.java
@@ -0,0 +1,19 @@
+package me.northpl93.utils;
+
+public class JsonUsersResponseParser
+{
+	private String templateHtml;
+	private String sidebarHtml;
+	
+	public JsonUsersResponseParser(){}
+	
+	public String getSidebarHtml()
+	{
+		return sidebarHtml;
+	}
+
+	public String getTemplateHtml()
+	{
+		return templateHtml;
+	}
+}
